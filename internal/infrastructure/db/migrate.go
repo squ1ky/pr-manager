204bfb1195a9bf59a88c36bf8c159e1b74df7e15
squ1ky <squ1ky@mail.ru>
2025-11-15 11:16:52 +0300

    bugfix: add forgotten import in migrate.go

--- internal/infrastructure/db/migrate.go
+++ internal/infrastructure/db/migrate.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"log/slog"
 )
 
